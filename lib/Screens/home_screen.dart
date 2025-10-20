33fdab38d8dfe9c0b3687bf7911ca6091ae552d8
Eranga <erangakavishanka@icloud.com>
2025-10-20 18:29:24 +0530

    developed upper part of the home screen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_shop_app/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(children: [
@@ -14,15 +16,65 @@ class HomeScreen extends StatelessWidget {
 
             children: [
               Container(
-                height: 300,
+                padding:EdgeInsets.symmetric(horizontal: 25 ),
+                height: 320,
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: 
-                  [
-                    Colors.grey.shade900,
-                    Colors.black.withAlpha(240)
-                  ])
+                  color: Colors.grey.shade900,
+                  // gradient: LinearGradient(colors: 
+                  // [
+                  //   Colors.grey.shade900,
+                  //   Colors.black.withAlpha(240)
+                  // ])
                 ),
+                child: SafeArea(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Text("Location", style: TextStyle(color: Colors.grey, fontSize: 16),),
+                    Row(children: [
+                      Text('Colombo, Sri Lanka', style: TextStyle(color: Colors.grey.shade200, fontSize: 18),),
+                      Icon(Icons.arrow_drop_down_rounded, color: Colors.grey.shade200,)
+                    
+
+                    ],),
+                    SizedBox(height: 12),
+                    Row(children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        height: 60,
+                        width: size.width * 0.68,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade800,
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Row(children: [
+                          Icon(Icons.search_rounded, color: Colors.white),
+                          SizedBox(width: 10),
+                          Text('Search Coffee', style: TextStyle(color: Colors.grey, fontSize: 16),)
+                        ],)
+
+                      ),
+
+                      Spacer(),
+
+                      Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: CustomColors.primaryColor,
+                          borderRadius: BorderRadius.circular(20)
+                      
+                        ),
+                      
+                      
+                      )
+
+                    ],)
+
+                    
+                  ],),
+                )
               ),
 
               Align(
@@ -30,12 +82,14 @@ class HomeScreen extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25),
                   child: Container(
-                    height: 200,
+                    height: 160,
                     width: double.infinity,
                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
                       color: Colors.grey,
                       image: 
                       DecorationImage(
+                        
                         image: AssetImage("assets/images/banner2.png"),
                         fit: BoxFit.cover
                   
